88533a2013a47028c1c2c3a25820c974582c03a1
Ethan Kennerly <ethankennerly@gmail.com>
2019-07-07 17:40:29 -0700

    Analytics Reporter: First mouse down.

--- TrumpOnTrial/Assets/Scripts/Analytics/AnalyticsReporter.cs
+++ TrumpOnTrial/Assets/Scripts/Analytics/AnalyticsReporter.cs
@@ -10,6 +10,8 @@ namespace FineGameDesign.Analytics
         [SerializeField]
         private bool m_DebugMode = false;
 
+        private bool m_FirstMouseDownReported = false;
+
         private void OnValidate()
         {
             AnalyticsEvent.debugMode = m_DebugMode;
@@ -18,6 +20,28 @@ namespace FineGameDesign.Analytics
         private void Awake()
         {
             AnalyticsEvent.debugMode = m_DebugMode;
+        }
+
+        private void Update()
+        {
+            UpdateFirstMouseDown();
+        }
+
+        private void UpdateFirstMouseDown()
+        {
+            if (m_FirstMouseDownReported)
+            {
+                return;
+            }
+            
+            if (!Input.GetMouseButtonDown(0))
+            {
+                return;
+            }
+
+            m_FirstMouseDownReported = true;
+
+            AnalyticsEvent.FirstInteraction();
         }
     }
 }
